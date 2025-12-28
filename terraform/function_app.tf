e149349435a4dabf22e7b9cdae2f6fa145607369
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-12-28 15:27:09 +0000

    feat: Switch from user-assigned to system-assigned identity for Linux function app

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -1,12 +1,3 @@
-resource "azurerm_user_assigned_identity" "app" {
-  for_each = toset(var.locations)
-
-  name                = format("id-platform-sitewatch-func-%s-%s", var.environment, lower(each.value))
-  location            = each.value
-  resource_group_name = local.sitewatch_resource_groups[each.value]
-  tags                = var.tags
-}
-
 resource "azurerm_linux_function_app" "app" {
   for_each = toset(var.locations)
 
@@ -27,8 +18,7 @@ resource "azurerm_linux_function_app" "app" {
   functions_extension_version = "~4"
 
   identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.app[each.value].id]
+    type = "SystemAssigned"
   }
 
   site_config {
@@ -54,8 +44,6 @@ resource "azurerm_linux_function_app" "app" {
     "portal_appinsights_connection_string"      = data.azurerm_application_insights.portal.connection_string
     "geolocation_appinsights_connection_string" = data.azurerm_application_insights.geolocation.connection_string
 
-    "AZURE_CLIENT_ID" = azurerm_user_assigned_identity.app[each.value].client_id
-
     "test_config" = jsonencode(var.availability_tests)
 
     "xtremeidiots_forums_task_key" = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.xtremeidiots_forums_task_key.name)
@@ -76,7 +64,7 @@ resource "azurerm_role_assignment" "app_to_keyvault" {
 
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_user_assigned_identity.app[each.value].principal_id
+  principal_id         = azurerm_linux_function_app.app[each.value].identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "app_to_storage_blob" {
@@ -84,5 +72,5 @@ resource "azurerm_role_assignment" "app_to_storage_blob" {
 
   scope                = azurerm_storage_account.function_app_storage[each.value].id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = azurerm_user_assigned_identity.app[each.value].principal_id
+  principal_id         = azurerm_linux_function_app.app[each.value].identity[0].principal_id
 }
