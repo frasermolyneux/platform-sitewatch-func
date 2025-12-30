4445c3f01581d999e055dd6fb15b4ca21c196655
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-12-30 20:42:20 +0000

    feat: Add Azure Monitor action groups and metric alerts for availability testing

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -8,4 +8,18 @@ locals {
     dev = 25
     prd = 75
   }
+
+  action_group_map = {
+    0 = data.azurerm_monitor_action_group.critical
+    1 = data.azurerm_monitor_action_group.high
+    2 = data.azurerm_monitor_action_group.moderate
+    3 = data.azurerm_monitor_action_group.low
+    4 = data.azurerm_monitor_action_group.informational
+  }
+
+  app_insights_map = {
+    "default"     = azurerm_application_insights.ai[var.locations[0]]
+    "portal"      = data.azurerm_application_insights.portal
+    "geolocation" = data.azurerm_application_insights.geolocation
+  }
 }
---
+++ terraform/monitor_metric_alerts.tf
@@ -0,0 +1,38 @@
+resource "azurerm_monitor_metric_alert" "availability" {
+  for_each = { for each in var.availability_tests : each.app => each }
+
+  name = "${each.value.workload}-${each.value.environment} - ${each.key} - availability"
+
+  resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
+  scopes              = [local.app_insights_map[each.value.app_insights].id]
+
+  description = "Availability test for ${each.key}"
+
+  criteria {
+    metric_namespace = "microsoft.insights/components"
+    metric_name      = "availabilityResults/availabilityPercentage"
+    aggregation      = "Average"
+    operator         = "LessThan"
+    threshold        = 95
+
+    dimension {
+      name     = "availabilityResult/name"
+      operator = "StartsWith"
+      values   = [each.key]
+    }
+  }
+
+  severity = each.value.severity
+
+  frequency   = "PT1M"
+  window_size = "PT30M"
+
+  action {
+    action_group_id = local.action_group_map[each.value.severity].id
+  }
+
+  tags = {
+    "Workload"    = each.value.workload
+    "Environment" = each.value.environment
+  }
+}
