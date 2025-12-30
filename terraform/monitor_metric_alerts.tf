1de0f0f73abf0393807bb6f6876ca038ecaead9b
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-12-30 20:46:44 +0000

    fix: Update resource_group_name format in monitor_metric_alerts.tf

--- terraform/monitor_metric_alerts.tf
+++ terraform/monitor_metric_alerts.tf
@@ -3,7 +3,7 @@ resource "azurerm_monitor_metric_alert" "availability" {
 
   name = "${each.value.workload}-${each.value.environment} - ${each.key} - availability"
 
-  resource_group_name = azurerm_resource_group.rg[var.locations[0]].name
+  resource_group_name = format("rg-platform-sitewatch-func-%s-%s", var.environment, var.locations[0])
   scopes              = [local.app_insights_map[each.value.app_insights].id]
 
   description = "Availability test for ${each.key}"
