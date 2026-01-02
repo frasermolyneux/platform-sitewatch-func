ef2aa42fcf10d2735fd97f5d475056a82de0cbf7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-02 09:12:59 +0000

    fix: Simplify action group handling in locals and monitor_metric_alerts for improved clarity

--- terraform/monitor_metric_alerts.tf
+++ terraform/monitor_metric_alerts.tf
@@ -1,7 +1,5 @@
 resource "azurerm_monitor_metric_alert" "availability" {
-  for_each = local.monitor_action_groups == null ? {} : {
-    for each in var.availability_tests : each.app => each if contains(keys(local.action_group_map), tostring(each.severity))
-  }
+  for_each = { for each in var.availability_tests : each.app => each }
 
   name = "${each.value.workload}-${each.value.environment} - ${each.key} - availability"
 
@@ -30,7 +28,7 @@ resource "azurerm_monitor_metric_alert" "availability" {
   window_size = "PT30M"
 
   action {
-    action_group_id = local.action_group_map[tostring(each.value.severity)].id
+    action_group_id = local.action_group_map[each.value.severity].id
   }
 
   tags = {
